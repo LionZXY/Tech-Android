b5696eeb700d2b9ef9fd2d759b4b54039cb66c5c
Punk_Off <pankov.kirill1997@yandex.ru>
2020-03-19 15:21:20 +0300

    Techopark.Android.Homework

---
+++ app/src/main/java/com/techopark/fragment_example/MainActivity.java
@@ -0,0 +1,25 @@
+package com.techopark.fragment_example;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment, new ListFragment());
+            fragmentTransaction.commit();
+        }
+
+    }
+}
